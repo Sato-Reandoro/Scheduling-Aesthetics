29013458f99610d1d9e21cf9f8362c330a883151
Leandro_soares <119981487+Sato-Reandoro@users.noreply.github.com>
2023-11-13 16:21:13 -0300

    repositorio que nn foi

--- Agendamentocrm/src/main/java/com/agendamento/crm/repository/FuncionariosRepository.java
+++ Agendamentocrm/src/main/java/com/agendamento/crm/repository/FuncionariosRepository.java
@@ -3,7 +3,7 @@ package com.agendamento.crm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.agendamento.crm.model.Admin;
+
 import com.agendamento.crm.model.Funcionarios;
 
 @Repository
